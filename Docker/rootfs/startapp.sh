cda6eeac99c77be1f82288f68e21a93a1eaa67da
Jorge Thomas (Akrista) <23145794+Akrista@users.noreply.github.com>
2023-04-11 21:32:56 -0400

    * 🐛 fix(startapp.sh): replace bash with sh and add error handling * ✨ feat(startapp.sh): add logging and graceful termination of Steam The script now uses sh instead of bash to improve compatibility. Error handling has been added to treat unset variables as an error. Logging has been added to provide more information about the script's execution. The script now gracefully terminates Steam when it receives a TERM, QUIT, or INT signal.

--- Docker/rootfs/startapp.sh
+++ Docker/rootfs/startapp.sh
@@ -1,4 +1,30 @@
-#!/bin/bash
-export HOME=/steam
-export LD_LIBRARY_PATH="/steam/xdg/data/Steam/ubuntu12_32"
-exec steam
+#!/usr/bin/with-contenv sh
+
+set -u # Treat unset variables as an error.
+
+trap "exit" TERM QUIT INT
+trap "kill_steam" EXIT
+
+log() {
+    echo "[steam] $*"
+}
+
+kill_steam() {
+    PID="$(pgrep steam)"
+    if [ "$PID" != "" ]; then
+        log "Terminating Steam..."
+        kill $PID
+        wait $PID
+    fi
+}
+
+if [ -z "${HOME+x}" ]; then
+    export HOME=/steam
+fi
+
+if [ -z "${LD_LIBRARY_PATH+x}" ]; then
+    export LD_LIBRARY_PATH="/steam/xdg/data/Steam/ubuntu12_32"
+fi
+
+log "Starting Steam..."
+exec steam >/dev/null 2>&1 &
